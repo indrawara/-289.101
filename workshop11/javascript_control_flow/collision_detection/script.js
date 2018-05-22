9f154f5599afc3efc0cefdd4d4792bdc846b0a42
Simoes Mires <19022418@wsom-072794.massey.ac.nz>
2018-05-22 16:03:40 +1200

    learn about using java script

--- workshop11/javascript_control_flow/collision_detection/script.js
+++ workshop11/javascript_control_flow/collision_detection/script.js
@@ -9,3 +9,57 @@ Next, add the following features:
 - create an item that makes the puck smaller
 
 */
+
+var puck_x = 210;
+var puck_y = 210;
+var speed = 50;
+var puck = document.getElementById('puck');
+var gap = document.getElementById('gap');
+
+function logCoordinates() {
+    console.log('x=' + puck_x + 'y='+ puck_y);
+}
+function checkCollison() {
+    if (puck_x > 250 && puck_x < 350 && puck_y > 150 && puck_y < 250) {
+        gap.style.backgroundColor = 'red';
+    }
+    else{
+        gap.style.backgroundColor = 'white'
+    }
+}
+
+
+puck.style.left = puck_x + 'px';
+puck.style.top = puck_y + 'px';
+puck
+
+// left
+document.getElementById('left').addEventListener('click', function(){
+    puck_x -= speed;
+    puck.style.left = puck_x + 'px';
+    logCoordinates();
+    checkCollison();
+}); 
+    
+//right 
+    
+document.getElementById('right').addEventListener('click', function(){
+    puck_x += speed;
+    puck.style.left = puck_x + 'px'; 
+    logCoordinates();
+    checkCollison();
+});
+//top 
+document.getElementById('up').addEventListener('click', function(){
+    puck_y -= speed;
+    puck.style.top = puck_y + 'px'; 
+    logCoordinates();
+    checkCollison();
+});
+//down
+document.getElementById('down').addEventListener('click', function(){
+    puck_y += speed;
+    puck.style.top = puck_y + 'px';
+    logCoordinates();
+    checkCollison();
+});
